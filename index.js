0be5c3230f475a2cb38fb37ca66038a55910afd7
Shahadat Hosen <shahadat74238@gmail.com>
2023-10-18 23:02:15 +0600

    Post method complect

--- index.js
+++ index.js
@@ -1,12 +1,59 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// watches-server
+// epL9aEV5pGHVuyAz
+
+
+
+const uri = "mongodb+srv://watches-server:epL9aEV5pGHVuyAz@cluster0.cxghft2.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const watchesCollection = client.db('watchesDB').collection('watches');
+
+    // insert watches in mongoDB.
+    app.post('/watches', async(req, res) => {
+      const watch = req.body;
+      console.log(watch);
+      const result = await watchesCollection.insertOne(watch);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
-  res.send('Time Tell site is Running.....!');
+  res.send('Watches site is Running.....!');
 })
 
 app.listen(port, () => {
-  console.log(`Time Tell site is Running on port ${port}`)
+  console.log(`Watches Tell site is Running on port ${port}`)
 })
