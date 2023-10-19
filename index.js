0f94ef907e3a24b1435b5b61a7fac93885efb6c4
Shahadat Hosen <shahadat74238@gmail.com>
2023-10-19 22:54:54 +0600

    find data usiing id and brand name

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const cors = require('cors');
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3001;
 
@@ -11,9 +11,8 @@ app.use(express.json());
 // watches-server
 // epL9aEV5pGHVuyAz
 
-
-
-const uri = "mongodb+srv://watches-server:epL9aEV5pGHVuyAz@cluster0.cxghft2.mongodb.net/?retryWrites=true&w=majority";
+const uri =
+  "mongodb+srv://watches-server:epL9aEV5pGHVuyAz@cluster0.cxghft2.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -21,7 +20,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -29,19 +28,88 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const watchesCollection = client.db('watchesDB').collection('watches');
+    const watchesCollection = client.db("watchesDB").collection("watches");
+    const brandsCollection = client.db("watchesDB").collection("brands");
 
     // insert watches in mongoDB.
-    app.post('/watches', async(req, res) => {
+    app.post("/watches", async (req, res) => {
       const watch = req.body;
       console.log(watch);
       const result = await watchesCollection.insertOne(watch);
       res.send(result);
     });
+    // get all the watch
+    app.get("/watches", async (req, res) => {
+      const query = watchesCollection.find();
+      const result = await query.toArray();
+      res.send(result);
+    });
+    //  get one watch
+    app.get("/watch/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const watches = await watchesCollection.findOne(query)
+      res.send(watches);
+    });
+    // get brand data
+    app.get("/watchBrand/:brand", async (req, res) => {
+      const brand = req.params.brand;
+      const query = { brand: brand };
+      const watches = await watchesCollection.find(query).toArray();
+      res.send(watches);
+    });
+
+    // insert all brands in mongoDB.
+    app.post("/brands", async (req, res) => {
+      const brands = [
+        {
+          image:
+            "https://media.wired.com/photos/6081f4280c9b5877078878e2/16:9/w_2399,h_1349,c_limit/business_plaintext_apple_1313768378.jpg",
+          name: "Rolex",
+        },
+        {
+          image:
+            "https://media.wired.com/photos/6081f4280c9b5877078878e2/16:9/w_2399,h_1349,c_limit/business_plaintext_apple_1313768378.jpg",
+          name: "Apple",
+        },
+        {
+          image:
+            "https://media.wired.com/photos/6081f4280c9b5877078878e2/16:9/w_2399,h_1349,c_limit/business_plaintext_apple_1313768378.jpg",
+          name: "Casio",
+        },
+        {
+          image:
+            "https://media.wired.com/photos/6081f4280c9b5877078878e2/16:9/w_2399,h_1349,c_limit/business_plaintext_apple_1313768378.jpg",
+          name: "Omega",
+        },
+        {
+          image:
+            "https://media.wired.com/photos/6081f4280c9b5877078878e2/16:9/w_2399,h_1349,c_limit/business_plaintext_apple_1313768378.jpg",
+          name: "Seiko",
+        },
+        {
+          image:
+            "https://media.wired.com/photos/6081f4280c9b5877078878e2/16:9/w_2399,h_1349,c_limit/business_plaintext_apple_1313768378.jpg",
+          name: "Citizen",
+        },
+      ];
+      const result = await brandsCollection.insertMany(brands);
+      res.send(result);
+    });
+
+    // get all the brands
+    app.get("/brands", async (req, res) => {
+      const query = brandsCollection.find();
+      const result = await query.toArray();
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -49,11 +117,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('Watches site is Running.....!');
-})
+app.get("/", (req, res) => {
+  res.send("Watches site is Running.....!");
+});
 
 app.listen(port, () => {
-  console.log(`Watches Tell site is Running on port ${port}`)
-})
+  console.log(`Watches Tell site is Running on port ${port}`);
+});
